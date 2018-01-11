9afc80a021f53a3b1965749365d06717e4f3b254
Yoan Ballesteros <b.yoan@live.fr>
2018-01-11 23:00:13 +0100

    First attempt of Entity usage in test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace Http\Controllers;
+
+use Entities\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class TestController extends Controller {
-
-    public function homeAction(Request $request, string $slug, string $slug2): Response
+class TestController extends Controller
+{
+    public function homeAction(Request $request, int $userId): Response
     {
+        $user = new User($userId);
         return $this->render('test.twig', array(
-            'test' => 'test'
+            'test' => 'test',
+            'user' => $user
         ));
     }
 
